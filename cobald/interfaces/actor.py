72f0f225fe16c051073865496a3acdc6d202f348
Max Fischer <maxfischer2781@gmail.com>
2018-05-07 18:06:48 +0200

    added actor interface

---
+++ cobald/interfaces/actor.py
@@ -0,0 +1,13 @@
+import abc
+import asyncio
+
+
+class Actor(abc.ABC):
+    """
+    An active component capable of concurrently performing work
+    """
+    def mount(self, event_loop: asyncio.BaseEventLoop):
+        event_loop.create_task(self.run())
+
+    async def run(self):
+        return
