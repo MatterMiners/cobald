52a3fca6ba8971ca36632b05488169fdd2a0f888
Max Fischer <maxfischer2781@gmail.com>
2018-05-08 13:05:21 +0200

    added proxy pool interface

--- cobald/interfaces/pool.py
+++ cobald/interfaces/pool.py
@@ -33,3 +33,33 @@ class Pool(abc.ABC):
     def consumption(self) -> float:
         """Fraction of the provided resources which is assigned for usage"""
         raise NotImplementedError
+
+
+@Pool.register
+class ProxyPool(abc.ABC):
+    @property
+    def supply(self):
+        """The volume of resources that is provided by this site"""
+        return self.target.supply
+
+    @property
+    def demand(self):
+        """The volume of resources to be provided by this site"""
+        return self.target.demand
+
+    @demand.setter
+    def demand(self, value):
+        self.target.demand = value
+
+    @property
+    def utilisation(self) -> float:
+        """Fraction of the provided resources which is actively used"""
+        return self.target.utilisation
+
+    @property
+    def consumption(self) -> float:
+        """Fraction of the provided resources which is assigned for usage"""
+        return self.target.consumption
+
+    def __init__(self, target: Pool):
+        self.target = target
