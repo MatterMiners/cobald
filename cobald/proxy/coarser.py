2d18ab569ff5c6ad5bf502d9b97a7fa39c2b4aed
Max Fischer <maxfischer2781@gmail.com>
2018-05-09 10:57:21 +0200

    added pool proxies for limits and granularity of demand

---
+++ cobald/proxy/coarser.py
@@ -0,0 +1,17 @@
+from cobald.interfaces.pool import ProxyPool, Pool
+
+
+class Coarser(ProxyPool):
+    """
+    Proxy limiting changes in a pool to a certain granularity
+
+    :param target: the pool to which changes are applied
+    :param granularity: granularity of changes
+    """
+    @ProxyPool.demand.setter
+    def demand(self, value):
+        self.target.demand = value // self.granularity * self.granularity
+
+    def __init__(self, target: Pool, granularity: int = 1):
+        super().__init__(target=target)
+        self.granularity = granularity
---
+++ cobald/proxy/limiter.py
@@ -0,0 +1,19 @@
+from cobald.interfaces.pool import ProxyPool, Pool
+
+
+class Limiter(ProxyPool):
+    """
+    A passive buffer limiting changes to a pool
+
+    :param target: the pool to which changes are applied
+    :param minimum: minimum demand to provide
+    :param maximum: maximum demand to provide
+    """
+    @ProxyPool.demand.setter
+    def demand(self, value):
+        self.target.demand = type(value)(min(self.maximum, max(self.minimum, value)))
+
+    def __init__(self, target: Pool, minimum: float = 1.0, maximum: float = float('inf')):
+        super().__init__(target=target)
+        self.minimum = minimum
+        self.maximum = maximum
