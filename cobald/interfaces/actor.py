dcc7bfafe604d94860fda6eb9628497a1a1c426a
Max Fischer <maxfischer2781@gmail.com>
2018-05-09 10:56:45 +0200

    type hint fix

--- cobald/interfaces/actor.py
+++ cobald/interfaces/actor.py
@@ -6,7 +6,7 @@ class Actor(abc.ABC):
     """
     An active component capable of concurrently performing work
     """
-    def mount(self, event_loop: asyncio.BaseEventLoop):
+    def mount(self, event_loop: asyncio.AbstractEventLoop):
         event_loop.create_task(self.run())
 
     async def run(self):
