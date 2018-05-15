0d3d4ed24847d4f72ddfcbe3b5bdf37e83c6c132
Max Fischer <maxfischer2781@gmail.com>
2018-05-15 13:37:54 +0200

    docs update

--- cobald/interfaces/actor.py
+++ cobald/interfaces/actor.py
@@ -4,9 +4,10 @@ import asyncio
 
 class Actor(metaclass=abc.ABCMeta):
     """
-    An active component capable of concurrently performing work
+    An active component that can be run by an event loop
     """
     def mount(self, event_loop: asyncio.AbstractEventLoop):
+        """Mount the Actor in ``event_loop`` for execution"""
         event_loop.create_task(self.run())
 
     @asyncio.coroutine
--- cobald/interfaces/controller.py
+++ cobald/interfaces/controller.py
@@ -5,7 +5,9 @@ from .pool import Pool
 
 class Controller(metaclass=abc.ABCMeta):
     """
-    Controller for the demand in a pool
+    Controller adjusting the demand in a :py:class:`~.Pool`
+
+    :param target: the resource pool for which demand is adjusted
     """
     def __init__(self, target: Pool):
         self.target = target
