55b1734a96756be1890faee2f72f63fa7e576f69
Max Fischer <maxfischer2781@gmail.com>
2018-05-15 18:19:12 +0200

    added metadata

---
+++ cobald/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'cobald'
+__summary__ = 'COBalD - the Opportunistic Balancing Deamon'
+__url__ = 'https://github.com/MaineKuehn/cobald'
+
+__version__ = '0.0.1'
+__author__ = 'Eileen Kuehn, Max Fischer'
+__email__ = 'maxfischer2781@gmail.com'
+__copyright__ = '2018 %s' % __author__
