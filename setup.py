9068eafb2e10fac3b49e499d97a889026e5b9c1d
Max Fischer <maxfischer2781@gmail.com>
2018-05-15 18:35:27 +0200

    added installation files

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+repo_base_dir = os.path.abspath(os.path.dirname(__file__))
+# pull in the packages metadata
+package_about = {}
+with open(os.path.join(repo_base_dir, "cobald", "__about__.py")) as about_file:
+    exec(about_file.read(), package_about)
+
+
+with open(os.path.join(repo_base_dir, 'README.rst'), 'r') as README:
+    long_description = README.read()
+
+if __name__ == '__main__':
+    setup(
+        name=package_about['__title__'],
+        version=package_about['__version__'],
+        description=package_about['__summary__'],
+        long_description=long_description.strip(),
+        author=package_about['__author__'],
+        author_email=package_about['__email__'],
+        url=package_about['__url__'],
+        packages=find_packages(),
+        # dependencies
+        install_requires=[],
+        # metadata for package search
+        license='MIT',
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Information Technology',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: System Administrators',
+            'Topic :: Adaptive Technologies',
+            'Topic :: Office/Business :: Scheduling',
+            'Topic :: System :: Distributed Computing',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
+        keywords=package_about['__keywords__'],
+        # unit tests
+        # test_suite='cobald_unittests',
+    )
