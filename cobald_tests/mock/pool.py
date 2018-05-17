c24bc55c3382676844d4f9a977fa0328406a9762
Eileen Kuehn <eileen.kuehn@kit.edu>
2018-05-17 22:31:41 +0200

    added a mocked pool for testing

--- cobald_tests/mock/pool.py
+++ cobald_tests/mock/pool.py
@@ -3,12 +3,31 @@ from cobald.interfaces.pool import Pool
 
 class MockPool(Pool):
     demand = 0
-    utilisation = 1.0
+
+    def __init__(self):
+        self._utilisation = 1.0
+        self._allocation = 1.0
 
     @property
     def supply(self):
         return self.demand
 
     @property
-    def consumption(self) -> float:
-        return self.utilisation
+    def allocation(self):
+        return self._allocation
+
+    @allocation.setter
+    def allocation(self, value):
+        if value < self._utilisation:
+            self._utilisation = value
+        self._allocation = value
+
+    @property
+    def utilisation(self):
+        return self._utilisation
+
+    @utilisation.setter
+    def utilisation(self, value):
+        if value > self._allocation:
+            self._allocation = value
+        self._utilisation = value
