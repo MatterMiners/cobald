51d130906eb49adf4448ec3538a4f580059749f7
Max Fischer <maxfischer2781@gmail.com>
2018-05-23 16:59:43 +0200

    added YAML parser requirement

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         url=package_about['__url__'],
         packages=find_packages(),
         # dependencies
-        install_requires=['typing'],
+        install_requires=['typing', 'pyyaml'],
         # metadata for package search
         license='MIT',
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
