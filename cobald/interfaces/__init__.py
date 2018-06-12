a82544de13cbaa3e27b9e042324905a72e2ec861
Max Fischer <maxfischer2781@gmail.com>
2018-06-12 10:28:59 +0200

    added overview docs for interfaces

---
+++ cobald/interfaces/__init__.py
@@ -0,0 +1,23 @@
+"""
+Interfaces for primitives of the :py:mod:`cobald` model
+
+Each :py:class:`~.pool.Pool` provides a varying number of resources.
+A :py:class:`~.controller.Controller` adjusts the number of resources that a :py:class:`~.pool.Pool` must provide.
+Several :py:class:`~.pool.Pool` can be combined in a single :py:class:`~.composite.CompositePool` to appear as one.
+To modify how a :py:class:`~.pool.Pool` presents or digests data,
+any number of :py:class:`~.proxy.PoolDecorator` may proceed it.
+
+.. graphviz::
+
+    digraph graphname {
+        graph [rankdir=LR, splines=lines, bgcolor="transparent"]
+        controller [label=Controller]
+        composite [label=CompositePool]
+        decoa [label=PoolDecorator]
+        poola, poolb [label=Pool]
+        controller -> decoa -> composite
+        composite -> poola
+        composite -> poolb
+    }
+
+"""
