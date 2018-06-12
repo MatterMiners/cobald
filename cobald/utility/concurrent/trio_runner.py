e874340dcb9226bb1480614f8e80ae2207d49470
Max Fischer <maxfischer2781@gmail.com>
2018-06-12 10:26:33 +0200

    trio runner listens to shutdown flag

--- cobald/utility/concurrent/trio_runner.py
+++ cobald/utility/concurrent/trio_runner.py
@@ -15,6 +15,16 @@ class TrioRunner(CoroutineRunner):
         trio.run(self.await_all)
 
     async def await_all(self):
+        self._running.set()
         with trio.open_nursery() as nursery:
-            for coroutine in self._coroutines:
-                nursery.start_soon(coroutine)
+            while self._running.is_set():
+                await self._start_outstanding(nursery=nursery)
+                await trio.sleep(1)
+        self._running.close()
+
+    async def _start_outstanding(self, nursery):
+        with self._lock:
+            for subroutine in self._payloads:
+                nursery.start_soon(subroutine)
+            self._payloads.clear()
+        await trio.sleep(0)
--- setup.py
+++ setup.py
@@ -23,7 +23,11 @@ if __name__ == '__main__':
         url=package_about['__url__'],
         packages=find_packages(),
         # dependencies
-        install_requires=['typing', 'pyyaml'],
+        install_requires=[
+            'typing',
+            'pyyaml',
+            'trio',
+        ],
         # metadata for package search
         license='MIT',
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
