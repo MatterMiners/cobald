bbde51b2504dd5e7b10567807b687c22fc2831e8
Max Fischer <maxfischer2781@gmail.com>
2018-06-15 13:57:11 +0200

    using module __doc__ from __about__

--- cobald/__about__.py
+++ cobald/__about__.py
@@ -1,3 +1,14 @@
+"""
+===========================================
+COBalD - the Opportunistic Balancing Daemon
+===========================================
+
+.. image:: https://readthedocs.org/projects/cobald/badge/?version=latest
+    :target: http://cobald.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+This is a **draft** for a feedback based balancing system for providing opportunistic resources.
+"""
 __title__ = 'cobald'
 __summary__ = 'COBalD - the Opportunistic Balancing Deamon'
 __url__ = 'https://github.com/MaineKuehn/cobald'
--- cobald/__init__.py
+++ cobald/__init__.py
@@ -1,1 +1,4 @@
-from .__about__ import __version__
+from . import __about__
+
+__version__ = __about__.__version__
+__doc__ = __about__.__doc__
