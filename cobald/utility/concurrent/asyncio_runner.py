c3cf9b68f9f066d2b5ba56bf18872b8e882c84db
Max Fischer <maxfischer2781@gmail.com>
2018-06-15 19:27:19 +0200

    asnycio runner now correctly starts coroutines

--- cobald/utility/concurrent/asyncio_runner.py
+++ cobald/utility/concurrent/asyncio_runner.py
@@ -7,5 +7,21 @@ class AsyncioRunner(CoroutineRunner):
     flavour = asyncio
 
     def run(self):
-        event_loop = asyncio.get_event_loop()
-        asyncio.wait(*self._payloads, loop=event_loop, return_when=asyncio.FIRST_EXCEPTION)
+        event_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(event_loop)
+        event_loop.create_task(self.await_all())
+        event_loop.run_forever()
+
+    async def await_all(self):
+        self._running.set()
+        while self._running.is_set():
+            await self._start_outstanding()
+            await asyncio.sleep(1)
+        self._running.close()
+
+    async def _start_outstanding(self):
+        with self._lock:
+            coroutines = self._payloads.copy()
+            self._payloads.clear()
+        if coroutines:
+            await asyncio.wait([coro() for coro in coroutines], return_when=asyncio.FIRST_EXCEPTION)
