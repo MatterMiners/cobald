67988e7715aea645488daf3cb66f13e3483658f6
Max Fischer <maxfischer2781@gmail.com>
2018-06-25 19:37:55 +0200

    added empty payload for termination tests

--- cobald_tests/utility/concurrent/test_meta_runner.py
+++ cobald_tests/utility/concurrent/test_meta_runner.py
@@ -24,10 +24,15 @@ class TestMetaRunner(object):
                 runner.run()
 
             def noop():
+                return
+
+            def loop():
                 while True:
                     time.sleep(0)
+
             runner = MetaRunner()
             runner.register_subroutine(subroutine=noop, flavour=flavour)
+            runner.register_subroutine(subroutine=loop, flavour=flavour)
             runner.register_subroutine(subroutine=abort, flavour=flavour)
             with pytest.raises(TerminateRunner):
                 runner.run()
@@ -43,10 +48,15 @@ class TestMetaRunner(object):
                 runner.run()
 
             async def noop():
+                return
+
+            async def loop():
                 while True:
                     await flavour.sleep(0)
             runner = MetaRunner()
+
             runner.register_coroutine(coroutine=noop, flavour=flavour)
+            runner.register_coroutine(coroutine=loop, flavour=flavour)
             runner.register_coroutine(coroutine=abort, flavour=flavour)
             with pytest.raises(TerminateRunner):
                 runner.run()
