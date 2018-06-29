dc1ccc9692f7ded46888cb310e52b016f1578642
Max Fischer <maxfischer2781@gmail.com>
2018-06-29 12:00:37 +0200

    adjusted JSON formatter to simpler log style

--- cobald/monitor/format_json.py
+++ cobald/monitor/format_json.py
@@ -18,45 +18,26 @@ class JsonFormatter(Formatter):
 
     :param fmt: default data for all records
     :param datefmt: format for timestamps
-    :param record_attributes: standard record attributes to include in output
 
-    The ``datefmt`` parameter has the same meaning as for :py:class:`~.Formatter`.
-    Note that if ``record_attributes`` does not whitelist ``"asctime"`` field,
-    the result is not included in the log output.
-
-    The ``record_attributes`` acts as a whitelist for the standard record attributes to include in the output.
-    See :py:attr:`~.RECORD_ATTRIBUTES` for a list of available attributes.
-    If ``record_attributes`` is a mapping, attributes are renamed according to it.
-    For example, ``record_attributes = {'message': 'identifier', 'asctime': 'timestamp'}`` whitelists the fields
-    ``'message'`` and ``asctime`` but renames them to ``'identifier'``  and ``'timestamp'``.
+    The ``datefmt`` parameter has almost the same meaning as for :py:class:`~.Formatter`.
+    Setting it to ``None`` uses the default time format.
+    However, setting it to any other value that is boolean false excludes the timestamp from reports.
     """
-    def __init__(self, fmt: dict = None, datefmt: str = None, record_attributes=('message',)):
+    def __init__(self, fmt: dict = None, datefmt: str = None):
         super().__init__(fmt=None, datefmt=datefmt, style='%')
         self._defaults = fmt or {}
         if not isinstance(self._defaults, Mapping):
             raise TypeError('`fmt` must be a Mapping or None')
-        self._ignore_attributes = {name for name in RECORD_ATTRIBUTES if name not in record_attributes}
-        try:
-            self._remap_attributes = {**record_attributes}
-        except TypeError:
-            self._remap_attributes = {}
-
-    def _as_dict(self, record):
-        """Transform a populated ``record`` to a :py:class:`dict`"""
-        data = {**self._defaults}
-        self__remap_attributes = self._remap_attributes
-        self__ignore_attributes = self._ignore_attributes
-        data.update({
-            self__remap_attributes.get(name, name): value
-            for name, value in record.__dict__.items()
-            if name not in self__ignore_attributes
-        })
-        return data
+        self._add_time = self.datefmt or self.datefmt is None
 
     def format(self, record: LogRecord):
-        record.asctime = self.formatTime(record, self.datefmt)
-        record.message = record.getMessage()
-        data = self._as_dict(record)
+        args = record.args
+        assert isinstance(args, Mapping), 'monitor record argument must be a mapping, not %r' % type(args)
+        data = self._defaults
+        if self._add_time:
+            data['time'] = self.formatTime(record, self.datefmt)
+        data['message'] = record.getMessage()
+        data.update(record.args)
         return json.dumps(data)
 
 
@@ -65,4 +46,4 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     logger.handlers = [logging.StreamHandler()]
     logger.handlers[0].formatter = JsonFormatter({'latitude': 49, 'longitude': 8})
-    logger.warning('forecast', extra={'temperature': 298, 'humidity': 0.45})
+    logger.warning('forecast', {'temperature': 298, 'humidity': 0.45})
