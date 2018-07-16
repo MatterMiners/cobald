24995e06837345dc5b3d2a19f4010398243fd8bc
Max Fischer <maxfischer2781@gmail.com>
2018-07-16 15:46:44 +0200

    removed coroutine and subroutine runner intermediate classes

--- cobald/utility/concurrent/base_runner.py
+++ cobald/utility/concurrent/base_runner.py
@@ -38,22 +38,6 @@ class BaseRunner(object):
         self.running.clear()
 
 
-class CoroutineRunner(BaseRunner):
-    """
-    Base Runner to concurrently execute coroutines
-    """
-    def register_coroutine(self, coroutine):
-        self._payloads.append(coroutine)
-
-
-class SubroutineRunner(BaseRunner):
-    """
-    Base Runner to concurrently execute subroutines
-    """
-    def register_subroutine(self, subroutine):
-        self._payloads.append(subroutine)
-
-
 class OrphanedReturn(Exception):
     """A runnable returned a value without anyone to receive it"""
     def __init__(self, who, value):
