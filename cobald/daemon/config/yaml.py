8a4d9d106f5d3b4192378218510b44bf68508e74
Max Fischer <maxfischer2781@gmail.com>
2018-08-02 13:51:14 +0200

    pipeline and logging configs are parsed separately

--- cobald/daemon/config/yaml.py
+++ cobald/daemon/config/yaml.py
@@ -7,14 +7,15 @@ def load_configuration(path, translator=Translator()):
     with open(path) as yaml_stream:
         config_data = load(yaml_stream)
     try:
-        logging_mapping = config_data['logging']
+        logging_mapping = config_data.pop('logging')
     except KeyError:
         pass
     else:
         configure_logging(logging_mapping)
     try:
-        root_pipeline = config_data['pipeline']
+        root_pipeline = config_data.pop('pipeline')
     except KeyError:
         raise FieldError('pipeline', 'configuration root')
     else:
+        assert not config_data, 'dangling configuration data: %s' % config_data
         return translator.translate_hierarchy({'pipeline': root_pipeline})
