844203bcb500072a469563913071afc05028865d
Max Fischer <maxfischer2781@gmail.com>
2018-08-03 16:45:15 +0200

    version bump and metadata update

--- cobald/__about__.py
+++ cobald/__about__.py
@@ -13,8 +13,8 @@ __title__ = 'cobald'
 __summary__ = 'COBalD - the Opportunistic Balancing Deamon'
 __url__ = 'https://github.com/MaineKuehn/cobald'
 
-__version__ = '0.1.0'
+__version__ = '0.9.0'
 __author__ = 'Eileen Kuehn, Max Fischer'
-__email__ = 'maxfischer2781@gmail.com'
+__email__ = 'mainekuehn@gmail.com'
 __copyright__ = '2018 %s' % __author__
 __keywords__ = 'opportunistic scheduling scheduler demand feedback-loop cobald'
