64dafd2e76f12f100357d6e3a567f90c41737661
Max Fischer <maxfischer2781@gmail.com>
2018-08-10 11:35:02 +0200

    added an assertion-like test with guaranteed behaviour

---
+++ cobald/utility/__init__.py
@@ -0,0 +1,19 @@
+class InvariantError(Exception):
+    """An invariant is violated"""
+
+
+def enforce(condition: bool, exception: BaseException = InvariantError()):
+    """
+    Enforce that ``condition`` is set by raising ``exception`` otherwise
+
+    This is a replacement for ``assert`` statements as part of validation.
+    It cannot be disabled with ``-O`` and may raise arbitrary exceptions.
+
+    .. code:: Python
+
+        def sqrt(value):
+            condition(value > 0, ValueError('value must be greater than zero')
+            return math.sqrt(value)
+    """
+    if not condition:
+        raise exception
