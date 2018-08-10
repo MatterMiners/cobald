b3fcf0f2001cb924fe556bb4c190fa970f8ea885
Max Fischer <maxfischer2781@gmail.com>
2018-08-10 11:33:51 +0200

    added controlable mock pool for tests

--- cobald_tests/mock/pool.py
+++ cobald_tests/mock/pool.py
@@ -31,3 +31,14 @@ class MockPool(Pool):
         if value > self._allocation:
             self._allocation = value
         self._utilisation = value
+
+
+class FullMockPool(Pool):
+    """Pool allowing to set every attribute"""
+    demand, supply, allocation, utilisation = 0, 0, 0, 0
+
+    def __init__(self, demand=0, supply=0, allocation=0.5, utilisation=0.5):
+        self.demand = demand
+        self.supply = supply
+        self.allocation = allocation
+        self.utilisation = utilisation
