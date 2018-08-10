c0a6ff5559c42eb4d4c554d34bd539f9509db115
Max Fischer <maxfischer2781@gmail.com>
2018-08-10 11:34:20 +0200

    added single decorator to combine limit, granularity and new backlog functionality

---
+++ cobald/decorator/standardizer.py
@@ -0,0 +1,50 @@
+from cobald.interfaces import Pool, PoolDecorator
+
+from ..utility import enforce
+from ..utility.primitives import infinity as inf
+
+
+class Standardiser(PoolDecorator):
+    """
+    Limits for changes to the demand of a pool
+
+    :param target: the pool on which changes are standardised
+    :param minimum: minimum ``target.demand`` allowed
+    :param maximum: maximum ``target.demand`` allowed
+    :param granularity: granularity of ``target.demand``
+    :param surplus: maximum by which ``target.supply`` may exceed ``target.demand``
+    :param backlog: maximum by which ``target.demand`` may exceed ``target.supply``
+
+    The default values apply no limits at all so that isolated limits may be used.
+    When several limits are set, ``granularity`` has the weakest priority,
+    both ``surplus`` and ``backlog`` may limit the result of ``granularity``,
+    and ``minimum`` and ``maximum`` overrule all other limits.
+    It is illegal to
+    """
+    @property
+    def demand(self):
+        return self.target.demand
+
+    @demand.setter
+    def demand(self, value):
+        minimum = max(self.minimum, self.target.supply - self.backlog)
+        maximum = min(self.maximum, self.target.supply + self.surplus)
+        request = value // self.granularity * self.granularity
+        self.target.demand = type(value)(min(maximum, max(minimum, request)))
+
+    def __init__(
+            self, target: Pool,
+            minimum: float = -inf, maximum: float = inf,
+            granularity: int = 1,
+            backlog: float = inf, surplus: float = inf,
+    ):
+        super().__init__(target)
+        enforce(minimum <= maximum, ValueError('minimum must be smaller than maximum'))
+        enforce(surplus > 0, ValueError('allowed surplus must be positive'))
+        enforce(backlog > 0, ValueError('allowed backlog must be positive'))
+        enforce(granularity > 0, ValueError('granularity must be positive'))
+        self.minimum = minimum
+        self.maximum = maximum
+        self.granularity = granularity
+        self.surplus = surplus
+        self.backlog = backlog
