f89ec35c51d666dd58601d13023104c6111ba051
Max Fischer <maxfischer2781@gmail.com>
2018-08-17 17:57:14 +0200

    added switch for controller selection on demand

---
+++ cobald/controller/switch.py
@@ -0,0 +1,49 @@
+from typing import Union
+
+import trio
+
+from ..interfaces import Pool, Controller
+from ..utility import enforce, InvariantError, pairwise
+from ..daemon.service import service
+
+
+@service(flavour=trio)
+class DemandSwitch(Controller):
+    """
+    Controller that dispatches to slaved controllers based on demand
+
+    .. code:: python
+
+        DemandSwitch(pool, linear_control, 10, supply_control)
+
+    :param target: the pool on which to regulate demand
+    """
+    def __init__(self, target: Pool, default: Controller, *slaves: Union[Controller, float], interval: int = 1):
+        super().__init__(target)
+        enforce(len(slaves) % 2 == 0, InvariantError("slaves must be paired with required demands"))
+        self._default = default
+        self._slaves = tuple(sorted(pairwise(slaves)))
+        enforce(
+            all((isinstance(demand, (int, float)) and isinstance(slave, Controller)) for demand, slave in self._slaves),
+            InvariantError("slaves must be paired with required demands (float, int)")
+        )
+        enforce(
+            default.target in (None, target) and all(slave.target in (None, target) for _, slave in self._slaves),
+            InvariantError("slaves must have None or same target as switch target")
+        )
+        default.target = target
+        for _, slave in self._slaves:
+            slave.target = target
+        self.interval = interval
+
+    async def run(self):
+        while True:
+            self.regulate_demand(self.interval)
+            await trio.sleep(self.interval)
+
+    def regulate(self, interval):
+        chosen = self._default
+        for demand, slave in self._slaves:
+            if demand <= self.target.demand:
+                chosen = slave
+        chosen.regulate(interval)
---
+++ cobald_tests/controller/test_switch.py
@@ -0,0 +1,22 @@
+import pytest
+
+from ..mock.pool import MockPool
+
+from cobald.controller.linear import LinearController
+from cobald.controller.switch import DemandSwitch
+
+
+class TestLinearController(object):
+    def test_select(self):
+        pool = MockPool()
+        switch_controller = DemandSwitch(pool, LinearController(pool, rate=1), 5, LinearController(pool, rate=2))
+        pool.utilisation = pool.allocation = 1.0
+        expected_demand = 0
+        for i in range(5):
+            switch_controller.regulate(1)
+            expected_demand += 1
+            assert(pool.demand == expected_demand)
+        for i in range(5):
+            assert(pool.demand == expected_demand)
+            switch_controller.regulate(1)
+            expected_demand += 2
