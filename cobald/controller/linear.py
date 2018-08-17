8a9f2d07072e0a24111b8702391efd9d8db32f0c
Max Fischer <maxfischer2781@gmail.com>
2018-08-17 18:19:52 +0200

    interval at which LinearController operates can be adjusted

--- cobald/controller/linear.py
+++ cobald/controller/linear.py
@@ -14,19 +14,21 @@ class LinearController(Controller):
     :param low_utilisation: pool utilisation below which resources are decreased
     :param high_allocation: pool allocation above which resources are increased
     :param rate: maximum change of demand in resources per second
+    :param interval: interval between adjustments in seconds
     """
-    def __init__(self, target: Pool, low_utilisation=0.5, high_allocation=0.5, rate=1):
+    def __init__(self, target: Pool, low_utilisation=0.5, high_allocation=0.5, rate=1, interval=1):
         super().__init__(target=target)
         assert rate > 0
         self.rate = rate
+        self.interval = interval
         assert low_utilisation <= high_allocation
         self.low_utilisation = low_utilisation
         self.high_allocation = high_allocation
 
     async def run(self):
         while True:
-            self.regulate_demand(1)
-            await trio.sleep(1)
+            self.regulate_demand(self.interval)
+            await trio.sleep(self.interval)
 
     def regulate(self, interval):
         if self.target.utilisation < self.low_utilisation:
