960b0fc7a69b36229409eb7f8e42784df2acbb72
Max Fischer <maxfischer2781@gmail.com>
2018-08-31 09:52:11 +0200

    added tests for logger decorator

---
+++ cobald_tests/decorator/test_logger.py
@@ -0,0 +1,46 @@
+import threading
+import logging
+import io
+
+import pytest
+
+from ..mock.pool import FullMockPool
+
+from cobald.decorator.logger import Logger
+
+
+class CapturingHandler(logging.StreamHandler):
+    @property
+    def content(self) -> str:
+        return self.stream.getvalue()
+
+    def __init__(self):
+        super().__init__(stream=io.StringIO())
+
+
+_test_index = 0
+_index_lock = threading.Lock()
+
+
+def make_logger(base_name: str = 'test_logger'):
+    global _test_index
+    with _index_lock:
+        log_name = base_name + '.test%d' % _test_index
+        _test_index += 1
+    logger = logging.getLogger(log_name)
+    logger.propagate = False
+    handler = CapturingHandler()
+    logger.handlers = [handler]
+    return logger, handler
+
+
+class TestLogger(object):
+    def test_transparent_logging(self):
+        pool = FullMockPool()
+        logger, handler = make_logger()
+        chain = Logger(target=pool, name=logger.name)
+        chain.demand += 1
+        assert handler.content
+        assert chain.demand == pool.demand
+        assert chain.utilisation == pool.utilisation
+        assert chain.allocation == pool.allocation
