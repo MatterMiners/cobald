ba2626d3701e3cbaf5d1cdf1e29f4dde6cd9d6a1
Max Fischer <maxfischer2781@gmail.com>
2018-12-05 18:22:35 +0100

    added concurrency guard/s

---
+++ cobald/utility/concurrent/guard.py
@@ -0,0 +1,27 @@
+import threading
+import functools
+
+
+def exclusive(via=threading.Lock):
+    """
+    Mark a callable as exclusive
+
+    :param via: factory for a Lock to guard the callable
+
+    Guards the callable against being entered again before completion.
+    Explicitly raises a :py:exc:`RuntimeError` on violation.
+    """
+    def make_exclusive(fnc):
+        fnc_guard = via()
+
+        @functools.wraps(fnc)
+        def exclusive_call(*args, **kwargs):
+            if fnc_guard.acquire(blocking=False):
+                try:
+                    return fnc(*args, **kwargs)
+                finally:
+                    fnc_guard.release()
+            else:
+                raise RuntimeError('exclusive call to %s violated')
+        return exclusive_call
+    return make_exclusive
