795497b324f16f504e4229325aff4d6db43febbc
matthias.schnepf <matthias.schnepf@kit.edu>
2019-01-30 11:03:46 +0100

    Add coldstart pools which have as default utilization and allocation 0 instead of 1

---
+++ cobald/composite/weighted_coldstart.py
@@ -0,0 +1,45 @@
+from ..interfaces import Pool, CompositePool
+
+
+class WeightedCompositeColdstart(CompositePool):
+    """
+    Weighted composition of several pools, with each pool weighted by its supply
+    """
+    children = []
+
+    @property
+    def demand(self):
+        return self._demand
+
+    @demand.setter
+    def demand(self, value):
+        self._demand = value
+        total_supply = self.supply
+        child_count = len(self.children)
+        for pool in self.children:
+            try:
+                pool.demand = value * pool.supply / total_supply
+            except ZeroDivisionError:
+                pool.demand = value / child_count
+
+    @property
+    def supply(self):
+        return sum(child.supply for child in self.children)
+
+    @property
+    def utilisation(self):
+        try:
+            return sum(child.utilisation * child.supply for child in self.children) / self.supply
+        except ZeroDivisionError:
+            return 0.
+
+    @property
+    def allocation(self):
+        try:
+            return sum(child.allocation * child.supply for child in self.children) / self.supply
+        except ZeroDivisionError:
+            return 0.
+
+    def __init__(self, *children: Pool):
+        self._demand = sum(child.demand for child in children)
+        self.children = list(children)
