63468e4007da0699c4cafe259b5f8bfef01260b1
Max Fischer <maxfischer2781@gmail.com>
2019-03-01 09:18:33 +0100

    added test for repeated currying

--- cobald_tests/interfaces/test_partial.py
+++ cobald_tests/interfaces/test_partial.py
@@ -29,3 +29,12 @@ class TestPartial(object):
         assert isinstance(pipeline.target, MockDecorator)
         assert isinstance(pipeline.target.target, MockDecorator)
         assert isinstance(pipeline.target.target.target, FullMockPool)
+
+    def test_recursive_curry(self):
+        partial_control = MockController.s()
+        assert isinstance(partial_control, Partial)
+        for _ in range(3):
+            partial_control = partial_control()
+            assert isinstance(partial_control, Partial)
+        pipeline = partial_control >> FullMockPool()
+        assert isinstance(pipeline, MockController)
