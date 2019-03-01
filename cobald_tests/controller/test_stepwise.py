1705913c71d052d6273f361e74986c377f28b694
Max Fischer <maxfischer2781@gmail.com>
2019-03-01 18:49:35 +0100

    added test for stepwise binding

--- cobald_tests/controller/test_stepwise.py
+++ cobald_tests/controller/test_stepwise.py
@@ -2,6 +2,8 @@ from types import FunctionType
 
 from cobald.controller.stepwise import Stepwise, UnboundStepwise, stepwise
 
+from ..mock.pool import FullMockPool
+
 
 class TestStepwise:
     def test_add(self):
@@ -17,3 +19,22 @@ class TestStepwise:
 
         assert isinstance(control, UnboundStepwise)
         assert isinstance(rule, FunctionType)
+
+    def test_instantiate(self):
+        @stepwise
+        def control(pool, interval):
+            ...
+
+        assert isinstance(control(FullMockPool()), Stepwise)
+        assert isinstance(control.s() >> FullMockPool(), Stepwise)
+        assert isinstance(control(FullMockPool(), interval=10), Stepwise)
+        assert isinstance(control.s(interval=10) >> FullMockPool(), Stepwise)
+
+        @control.add(supply=20)
+        def rule(pool, interval):
+            ...
+
+        assert isinstance(control(FullMockPool()), Stepwise)
+        assert isinstance(control.s() >> FullMockPool(), Stepwise)
+        assert isinstance(control(FullMockPool(), interval=10), Stepwise)
+        assert isinstance(control.s(interval=10) >> FullMockPool(), Stepwise)
