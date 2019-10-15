8621f3b3313521f10ad2a1881a30249affdcceff
Max Fischer <maxfischer2781@gmail.com>
2019-10-15 15:09:30 +0200

    testing guard for missing configuration sections

--- cobald_tests/daemon/core/test_config.py
+++ cobald_tests/daemon/core/test_config.py
@@ -34,7 +34,7 @@ class TestYamlConfig:
             assert True
 
     def test_load_dangling(self):
-        """Load a YAML config with dangling content"""
+        """Forbid loading a YAML config with dangling content"""
         with NamedTemporaryFile(suffix='.yaml') as config:
             with open(config.name, 'w') as write_stream:
                 write_stream.write(
@@ -46,6 +46,20 @@ class TestYamlConfig:
                         - !MockPool
                     random_things:
                         foo: bar
+                    """
+                )
+            with pytest.raises(ConfigurationError):
+                with load(config.name):
+                    assert False
+
+    def test_load_missing(self):
+        """Forbid loading a YAML config with missing content"""
+        with NamedTemporaryFile(suffix='.yaml') as config:
+            with open(config.name, 'w') as write_stream:
+                write_stream.write(
+                    """
+                    logging:
+                        version: 1.0
                     """
                 )
             with pytest.raises(ConfigurationError):
