e03fe56052c719063e8a5019467df4e7e052e350
Max Fischer <maxfischer2781@gmail.com>
2021-03-22 19:28:22 +0100

    documented dependency on extension

--- src/cobald/daemon/config/python.py
+++ src/cobald/daemon/config/python.py
@@ -1,3 +1,4 @@
+import pathlib
 import importlib.util
 import sys
 
@@ -6,12 +7,19 @@ _loaded = -1
 
 
 def load_configuration(path):
+    """
+    Load a configuration from a module stored at ``path``
+
+    The ``path`` must end in a valid file extension for the appropriate module type,
+    such as ``.py`` or ``.pyc`` for a plaintext or bytecode python module.
+    """
     global _loaded
     current_index = _loaded = _loaded + 1
     module_name = f"<cobald config {current_index}>"
     spec = importlib.util.spec_from_file_location(module_name, path)
     if spec is None:
-        raise RuntimeError(f"Failed to load {path}: no import spec could be created")
+        extension = pathlib.Path(path).suffix
+        raise RuntimeError(f"Failed to load {path} of type {extension}")
     module = importlib.util.module_from_spec(spec)
     sys.modules[module_name] = module
     spec.loader.exec_module(module)
