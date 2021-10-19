1e70906772831c469d232c17a23dabeb6d9760ec
Max Fischer <maxfischer2781@gmail.com>
2021-10-19 12:23:08 +0200

    unittest for #99

--- cobald_tests/daemon/core/test_config.py
+++ cobald_tests/daemon/core/test_config.py
@@ -1,6 +1,7 @@
 from tempfile import NamedTemporaryFile
 
 import pytest
+from contextlib import contextmanager
 
 from cobald.daemon.config.mapping import ConfigurationError
 from cobald.daemon.core.config import load, COBalDLoader, yaml_constructor
@@ -11,6 +12,38 @@ from ...mock.pool import MockPool
 
 # register test pool as safe for YAML configurations
 COBalDLoader.add_constructor(tag="!MockPool", constructor=yaml_constructor(MockPool))
+
+
+class TagTracker:
+    """Helper to track the invocation of YAML !Tags"""
+    count = -1
+
+    def __init__(self, *args, **kwargs):
+        type(self).count = self.count = type(self).count + 1
+        self.args = args
+        self.kwargs = kwargs
+
+    @classmethod
+    @contextmanager
+    def scope(cls, base_count=-1):
+        old_count, cls.count = cls.count, base_count
+        try:
+            yield
+        finally:
+            cls.count = old_count
+
+
+COBalDLoader.add_constructor(
+    tag="!TagTracker", constructor=yaml_constructor(TagTracker)
+)
+
+
+def get_config_section(config: dict, section: str):
+    return next(
+        content
+        for plugin, content in config.items()
+        if plugin.section == section
+    )
 
 
 class TestYamlConfig:
@@ -95,10 +128,26 @@ class TestYamlConfig:
                     """
                 )
             with load(config.name) as config:
-                pipeline = next(
-                    content
-                    for plugin, content in config.items()
-                    if plugin.section == "pipeline"
-                )
+                pipeline = get_config_section(config, "pipeline")
                 assert isinstance(pipeline[0], LinearController)
                 assert isinstance(pipeline[0].target, MockPool)
+
+    def test_load_tags_deep(self):
+        """Load !Tags with deep structure"""
+        with NamedTemporaryFile(suffix=".yaml") as config:
+            with open(config.name, "w") as write_stream:
+                write_stream.write(
+                    """
+                    pipeline:
+                        - !TagTracker
+                          kwarg:
+                            - nested: true
+                    """
+                )
+            with load(config.name) as config:
+                tagged = get_config_section(config, "pipeline")[0]
+                assert isinstance(tagged, TagTracker)
+                assert isinstance(tagged.kwargs['kwarg'], list)
+                assert isinstance(tagged.kwargs['kwarg'][0], dict)
+                assert tagged.kwargs['kwarg'][0].get("nested") is True
+                print(tagged.args)
