0ee3239136b4d0e5c83a014d7cd7bac07502f0a0
Max Fischer <maxfischer2781@gmail.com>
2022-03-01 14:13:41 +0100

    removed queueing and sync from base runner

--- src/cobald/daemon/runners/base_runner.py
+++ src/cobald/daemon/runners/base_runner.py
@@ -12,74 +12,57 @@ class BaseRunner(object):
         self._logger = logging.getLogger(
             "cobald.runtime.runner.%s" % NameRepr(self.flavour)
         )
-        self._payloads = []
-        self._lock = threading.Lock()
-        #: signal that runner should keep in running
-        self.running = threading.Event()
-        #: signal that runner has stopped
+        #: signal that runner is stopped
         self._stopped = threading.Event()
-        self.running.clear()
         self._stopped.set()
-
-    def __bool__(self):
-        with self._lock:
-            return bool(self._payloads) or self.running.is_set()
 
     def register_payload(self, payload):
         """
-        Register ``payload`` for asynchronous execution
+        Register ``payload`` for background execution in a threadsafe manner
 
         This runs ``payload`` as an orphaned background task as soon as possible.
         It is an error for ``payload`` to return or raise anything without handling it.
         """
-        with self._lock:
-            self._payloads.append(payload)
+        raise NotImplementedError
 
     def run_payload(self, payload):
         """
-        Register ``payload`` for synchronous execution
+        Register ``payload`` for direct execution in a threadsafe manner
 
         This runs ``payload`` as soon as possible, blocking until completion.
         Should ``payload`` return or raise anything, it is propagated to the caller.
         """
         raise NotImplementedError
 
-    def run(self):
+    async def run(self):
         """
-        Execute all current and future payloads
+        Execute all current and future payloads in an `asyncio` task
 
         Blocks and executes payloads until :py:meth:`stop` is called.
         It is an error for any orphaned payload to return or raise.
+
+        Implementations should override :py:meth:`~.run_payloads`
+        to customize their specific parts.
         """
         self._logger.info("runner started: %s", self)
+        self._stopped.clear()
         try:
-            with self._lock:
-                assert not self.running.is_set() and self._stopped.is_set(), (
-                    "cannot re-run: %s" % self
-                )
-                self.running.set()
-                self._stopped.clear()
-            self._run()
+            await self.run_payloads()
         except Exception:
             self._logger.exception("runner aborted: %s", self)
             raise
         else:
             self._logger.info("runner stopped: %s", self)
         finally:
-            with self._lock:
-                self.running.clear()
-                self._stopped.set()
+            self._stopped.set()
 
-    def _run(self):
+    async def run_payloads(self):
         raise NotImplementedError
 
     def stop(self):
-        """Stop execution of all current and future payloads"""
-        if not self.running.wait(0.2):
-            return
-        self._logger.debug("runner disabled: %s", self)
-        with self._lock:
-            self.running.clear()
+        """
+        Stop execution of all current and future payloads and wait for success
+        """
         self._stopped.wait()
 
 
