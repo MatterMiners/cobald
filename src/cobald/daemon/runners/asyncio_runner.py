4a96cf4e4cb7a5362c0fe95b2e84e9e81ef8b83c
Max Fischer <maxfischer2781@gmail.com>
2022-03-01 15:19:32 +0100

    changed AsyncioRunner to asyncio implementation

--- src/cobald/daemon/runners/asyncio_runner.py
+++ src/cobald/daemon/runners/asyncio_runner.py
@@ -1,9 +1,8 @@
-from typing import Optional
+from typing import Optional, Callable, Awaitable
 import asyncio
-from functools import partial
 
 from .base_runner import BaseRunner
-from .async_tools import raise_return, AsyncExecution
+from .async_tools import raise_return, ensure_coroutine
 
 
 class AsyncioRunner(BaseRunner):
@@ -16,67 +15,42 @@ class AsyncioRunner(BaseRunner):
         self.event_loop = loop if loop is not None else asyncio.new_event_loop()
         self._tasks = set()
 
-    def register_payload(self, payload):
-        super().register_payload(partial(raise_return, payload))
+    def register_payload(self, payload: Callable[[], Awaitable]):
+        self.event_loop.call_soon_threadsafe(
+            lambda: self._tasks.add(self.event_loop.create_task(raise_return(payload)))
+        )
 
-    def run_payload(self, payload):
-        execution = AsyncExecution(payload)
-        super().register_payload(execution.coroutine)
-        return execution.wait()
+    def run_payload(self, payload: Callable[[], Awaitable]):
+        future = asyncio.run_coroutine_threadsafe(
+            ensure_coroutine(payload()), self.event_loop
+        )
+        return future.result()
 
-    def _run(self):
-        asyncio.set_event_loop(self.event_loop)
-        self.event_loop.run_until_complete(self._run_payloads())
+    async def manage_payloads(self):
+        # Remove tracked tasks and raise if tasks leak
+        while self._tasks or self._running.is_set():
+            # let asyncio efficiently wait for errors
+            # we only force wake up via timeout every now and then to clean up
+            done, pending = await asyncio.wait(
+                self._tasks, timeout=60, return_when=asyncio.FIRST_EXCEPTION
+            )
+            self._tasks.difference_update(done)
+            for task in done:
+                # re-raise any exceptions
+                task.result()
 
-    async def _run_payloads(self):
-        """Async component of _run"""
-        delay = 0.0
-        try:
-            while self.running.is_set():
-                await self._start_payloads()
-                await self._reap_payloads()
-                await asyncio.sleep(delay)
-                delay = min(delay + 0.1, 1.0)
-        except Exception:
-            await self._cancel_payloads()
-            raise
-
-    async def _start_payloads(self):
-        """Start all queued payloads"""
-        with self._lock:
-            for coroutine in self._payloads:
-                task = self.event_loop.create_task(coroutine())
-                self._tasks.add(task)
-            self._payloads.clear()
-        await asyncio.sleep(0)
-
-    async def _reap_payloads(self):
-        """Clean up all finished payloads"""
-        for task in self._tasks.copy():
-            if task.done():
-                self._tasks.remove(task)
-                if task.exception() is not None:
-                    raise task.exception()
-        await asyncio.sleep(0)
-
-    async def _cancel_payloads(self):
-        """Cancel all remaining payloads"""
-        for task in self._tasks:
-            task.cancel()
-            await asyncio.sleep(0)
-        for task in self._tasks:
-            while not task.done():
-                await asyncio.sleep(0.1)
+    async def aclose(self):
+        self._running.clear()
+        while self._tasks:
+            for task in self._tasks.copy():
+                if task.done():
+                    self._tasks.remove(task)
                 task.cancel()
+            await asyncio.sleep(0.5)
 
     def stop(self):
-        if not self.running.wait(0.2):
+        if not self._running.wait(0.2):
             return
-        self._logger.debug("runner disabled: %s", self)
-        with self._lock:
-            self.running.clear()
-            for task in self._tasks:
-                task.cancel()
-        self._stopped.wait()
-        self.event_loop.stop()
-        self.event_loop.close()
+        # the loop exists independently of this runner, we can use it to shut down
+        closed = asyncio.run_coroutine_threadsafe(self.aclose(), self.event_loop)
+        closed.result()
