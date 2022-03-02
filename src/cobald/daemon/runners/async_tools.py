1d689e7f548cf9c45a32adc53e16e027491287ce
Max Fischer <maxfischer2781@gmail.com>
2022-03-02 13:56:45 +0100

    removed dead code

--- src/cobald/daemon/runners/async_tools.py
+++ src/cobald/daemon/runners/async_tools.py
@@ -20,35 +20,3 @@ def ensure_coroutine(awaitable: Awaitable) -> Coroutine:
         return await awaitable
 
     return wrapper()
-
-
-class AsyncExecution(object):
-    def __init__(self, payload):
-        self.payload = payload
-        self._result = None
-        self._done = threading.Event()
-        self._done.clear()
-
-    # explicit coroutine for libraries that type check
-    async def coroutine(self):
-        await self
-
-    def __await__(self):
-        try:
-            value = yield from self.payload().__await__()
-        except Exception as err:
-            self._result = None, err
-        else:
-            self._result = value, None
-        self._done.set()
-
-    def wait(self):
-        self._done.wait()
-        value, exception = self._result
-        if exception is None:
-            return value
-        else:
-            raise exception
-
-    def __repr__(self):
-        return "%s(%s)" % (self.__class__.__name__, self.payload)
