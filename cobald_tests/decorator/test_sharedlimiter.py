2807406f0a420cbe5d3ee02a8481675ff9747cff
Ughur Mammadzada <ughur.mammadzada@stud.uni-goettingen.de>
2026-02-09 12:58:41 +0100

    test load < threshold or is 0

--- cobald_tests/decorator/test_sharedlimiter.py
+++ cobald_tests/decorator/test_sharedlimiter.py
@@ -24,6 +24,12 @@ default_inputs = {
     "db_global_max_default": 100.0
 }
 
+other_pool_inputs = {
+    "db_pool_id": "Other",
+    "db_resource_id": "cpu",
+    "db_weight": 1.0,
+}
+
 def _db_con(db_path):
     con = sqlite3.connect(db_path)
     return con
@@ -47,6 +53,28 @@ def _set_upper_limit(db_inputs, resource_id: str, upper: float):
         db_inputs["db_path"],
         f"UPDATE limits SET upper_limit = {upper} WHERE feature = '{resource_id}'"
     )
+
+def _update_or_insert_pool_row(db_inputs, db_resource_id: str, db_pool_id: str, db_weight: float, usage: float):
+    _db_exec(
+        db_inputs["db_path"],
+        f"UPDATE {db_resource_id} SET weight={db_weight}, usage={usage} WHERE id='{db_pool_id}'"
+    )
+
+    db_path = db_inputs["db_path"]
+
+    con = _db_con(db_path)
+
+    try:
+        cur = con.cursor()
+        cur.execute(f"SELECT id FROM {db_resource_id} WHERE id='{db_pool_id}'")
+        row = cur.fetchone()
+        if row is None:
+            cur.execute(
+                f"INSERT INTO {db_resource_id}(id, weight, usage) VALUES ('{db_pool_id}', {db_weight}, {usage})"
+            )
+            con.commit()
+    finally:
+        con.close()
 
 @pytest.fixture(autouse=True)
 def clean_local_test_db():
@@ -91,4 +119,36 @@ class TestSharedLimiter(object):
             _set_upper_limit(db_input, default_inputs["db_resource_id"], 0.0)
 
             assert limiter.utilisation == 0
+
+    def test_utilisation_below_threshold_passthrough(self):
+        pool = FullMockPool()
+        pool.utilisation = 0.77
+        pool.supply = 0.0
+
+        for db_input in db_inputs:
+            limiter = SharedLimiter(pool, **db_input, **default_inputs, threshold=0.9)
+
+            # ensure total_usage/limit <= threshold
+            # our own row will be overwritten with supply on property access; keep supply 0 => my usage 0.
+            _update_or_insert_pool_row(db_input, **other_pool_inputs, usage=10.0) # total usage = 10
+            # load = 10/100 = 0.1 <= 0.9
+
+            got = limiter.utilisation
+            assert got == pool.utilisation
+
+    def test_utilisation_usage_zero_passthrough(self):
+        pool = FullMockPool()
+        pool.utilisation = 0.77
+        pool.supply = 0.0
+
+        for db_input in db_inputs:
+            limiter = SharedLimiter(pool, **db_input, **default_inputs, threshold=0.9)
+
+            # ensure total_usage/limit <= threshold
+            # our own row will be overwritten with supply on property access; keep supply 0 => my usage 0.
+            _update_or_insert_pool_row(db_input, **other_pool_inputs, usage=0.0) # total usage = 0
+            # load = 0
+
+            got = limiter.utilisation
+            assert got == pool.utilisation
 
